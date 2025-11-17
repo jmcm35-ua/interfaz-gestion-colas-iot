151bd78f38e022f566698ee7e74b7ccde0d3d402
Jose Manuel Canovas Manzano <jmcm35@alu.ua.es>
2025-11-17 10:47:34 +0100

    Creacion del proyecto en angular y primera estructuracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
